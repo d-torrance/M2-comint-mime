357c4e7e879dd02bbf78a2f5e0b0ef965025b8bd
Doug Torrance <dtorrance@piedmont.edu>
2023-12-22 12:32:04 -0500

    Load a package with code rather than running code directly
    Also gives the user the option to switch back to using the old
    show(URL).

---
+++ ComintMime.m2
@@ -0,0 +1,20 @@
+newPackage "ComintMime"
+
+export {
+    "restoreShow",
+    "updateShow"}
+
+oldshow = lookup(show, URL)
+newshow = url -> (
+    if (m := regex("^file://(.*)", first url)) =!= null
+    then (
+	file := substring(m#1, first url);
+	type := first lines get("!file -b --mime " | file);
+	print("\e]5151;{\"type\": \"" | type | "\"}\n" |
+	    first url | "\e\\\n"))
+    else oldshow url);
+
+updateShow = () -> (show URL := newshow;)
+restoreShow = () -> (show URL := oldshow;)
+
+updateShow()
--- M2-comint-mime.el
+++ M2-comint-mime.el
@@ -4,18 +4,13 @@
 (require 'M2)
 
 (defun M2-comint-mime-setup ()
-  (comint-send-string (car (M2--get-send-to-buffer))
-		      "printerr \"Setting up M2-comint-mime!\"
-oldshow = lookup(show, URL);
-show URL := url -> (
-    if (m := regex(\"^file://(.*)\", first url)) =!= null
-    then (
-	file := substring(m#1, first url);
-	type := first lines get(\"!file -b --mime \" | file);
-	print(\"\\033]5151;{\\\"type\\\": \\\"\" | type | \"\\\"}\\n\" |
-	    first url | \"\\033\\\\\\n\"))
-    else oldshow url);
-"))
+  (with-current-buffer (car (M2--get-send-to-buffer))
+    (end-of-buffer)
+    (insert (concat
+	     "needsPackage(\"ComintMime\", FileName => \""
+	     (file-name-directory (symbol-file 'M2-comint-mime))
+	     "ComintMime.m2\")"))
+    (comint-send-input)))
 
 (push '(M2-comint-mode . M2-comint-mime-setup) comint-mime-setup-function-alist)
 
