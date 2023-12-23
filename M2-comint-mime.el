849c82c67f9bfc0ddf1e956c386f3ae3545a68a1
Doug Torrance <dtorrance@piedmont.edu>
2023-12-23 13:43:34 -0500

    Use (goto-char (point-max)) instead of (end-of-buffer)

--- M2-comint-mime.el
+++ M2-comint-mime.el
@@ -34,7 +34,7 @@
 (defun M2-comint-mime-setup ()
   "Setup code for `comint-mime-setup' in `M2-comint-mode' buffers."
   (with-current-buffer (car (M2--get-send-to-buffer))
-    (end-of-buffer)
+    (goto-char (point-max))
     (insert (concat
 	     "needsPackage(\"ComintMime\", FileName => \""
 	     (file-name-directory (symbol-file 'M2-comint-mime))
