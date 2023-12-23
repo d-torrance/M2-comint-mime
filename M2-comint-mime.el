3ac795ab7841a23a75e88425779e78b12db0f3f8
Doug Torrance <dtorrance@piedmont.edu>
2023-12-22 21:58:11 -0500

    Add header/footer comments for .el file

--- M2-comint-mime.el
+++ M2-comint-mime.el
@@ -1,4 +1,32 @@
-;; usual header stuff
+;;; M2-comint-mime.el --- comint-mime for Macaulay2 -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023 Doug Torrance
+
+;; Author: Doug Torrance <dtorrance@piedmont.edu>
+;; Created: 2023-12-22
+;; Version: 0.1
+;; Keywords: processes, multimedia
+;; URL: https://github.com/d-torrance/M2-comint-mime
+;; Package-Requires: (comint-mime M2)
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; TODO
+
+;;; Code:
 
 (require 'comint-mime)
 (require 'M2)
@@ -16,3 +44,5 @@
 (push '(M2-comint-mode . M2-comint-mime-setup) comint-mime-setup-function-alist)
 
 (provide 'M2-comint-mime)
+
+;;; M2-comint-mime.el ends here
